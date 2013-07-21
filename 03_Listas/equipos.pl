a26754e78b30b82da5eef6ec5a619d09349e8092
Santiago Della Torre <sdellatorre@gmail.com>
2013-07-21 00:57:27 -0300

    Trabajo sobre carpeta Listas

--- 03_Listas/equipos.pl
+++ 03_Listas/equipos.pl
@@ -1,15 +1,12 @@
-contiene([X|_], X) .
-contiene([_|T], X) :- contiene(T,X).
-
 grandes([boca, river, sanlorenzo, independiente, racing]).
-esGrande(Equipo) :- grandes(Grandes), contiene(Grandes, Equipo).
+esGrande(Equipo) :- grandes(Grandes), member(Equipo, Grandes).
 
 jugador(maradona, [argentinos, boca, newells]).
 jugador(palermo, [estudiantes, boca]).
 jugador(sponton, [platente, river, lanus, espaniol]).
 jugador(francescoli, [river]).
 
-jugoEn(Jugador, Equipo) :- jugador(Jugador, Equipos), contiene(Equipos, Equipo).
+jugoEn(Jugador, Equipo) :- jugador(Jugador, Equipos), member(Equipo, Equipos).
 
 ganoLibertadores(boca, [1977, 1978, 2000, 2001, 2003, 2007]).
 ganoLibertadores(independiente, [1964, 1965, 1972, 1973, 1974, 1975, 1984]).
@@ -20,7 +17,7 @@ ganoLibertadores(velez, [1994]).
 ganoLibertadores(argentinos, [1984]).
 
 campeonesDelMundo([boca, independiente, estudiantes, river, racing, velez]).
-ganoIntercontinental(Equipo) :- campeonesDelMundo(Campeones), contiene(Campeones, Equipo).
+ganoIntercontinental(Equipo) :- campeonesDelMundo(Campeones), member(Equipo, Campeones).
 
 noJugoB(boca).
 jugoEnLaB(X) :- not(noJugoB(X)).
--- 03_Listas/tenistas.pl
+++ 03_Listas/tenistas.pl
@@ -1,24 +1,22 @@
-Enunciado:
-Con un conjunto de datos dados como una lista de tenistas que participan de un torneo, donde cada tenista es se represnta con una lista que contiene (nombre, ranking, nacionalidad) escriba las consultas que:
-- devuelva una lista con los nombres de los tenistas argentinos
-- devuelva el nombre del numero uno del ranking
-- devuelva la cantida de participantes del torneo.
+% Enunciado:
+% Con un conjunto de datos dados como una lista de tenistas que participan 
+% de un torneo, donde cada tenista es se represnta con una lista que 
+% contiene (nombre, ranking, nacionalidad) escriba las consultas que:
+%  - devuelva una lista con los nombres de los tenistas argentinos
+%  - devuelva el nombre del numero uno del ranking
+%  - devuelva la cantidad de participantes del torneo.
 
-
-
-Resolucion:
 tenistas( [[delpotro,8,argentino], [federer, 2, suizo], [yojovick,1 ,croata], [nalbandian, 25,argentino]]).
 
-iesimo(1, [C|_], C).
-iesimo(N, [_|T], X) :- N1 is N -1, iesimo(N1,T,X).
 
-uno(Nombre) :- tenistas(L), member(X,L), iesimo(2, X, 1), iesimo(1,X,Nombre).
+uno(Nombre) :- tenistas(L), member(DatosTenista, L), DatosTenista = [Nombre, 1, _].
 
-todos(X) :- tenistas(L), member(X,L).
+contar([], 0).
+contar([H|T], Cant) :- contar(T, X), Cant is X + 1.
+cantidad(Cantidad) :- tenistas(L), contar(L, Cantidad).
 
+arg(Argentinos) :- tenistas(L), arg(L, Argentinos).
 arg([],[]).
-arg([HL|TL], [X|T]) :- iesimo(3, HL, argentino), iesimo(1, HL, X), arg(TL,T).
-arg([_|TL],   T)  :- arg(TL,T).
-
-
+arg([HL|TL], [Nombre|T]) :- HL = [Nombre,_, argentino], arg(TL, T).
+arg([HL|TL], T) :- HL = [_,_, Nacionalidad], Nacionalidad \= argentino, arg(TL, T).
 
